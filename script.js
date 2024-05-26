7702ddd0e6527885adbd4ba0e6a2373bbe835b12
statusvisualeffect <154653760+statusvisualeffect@users.noreply.github.com>
2024-05-26 01:07:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('installButton').addEventListener('click', function () {
-	// Replace 'your-install-link' with the actual link you want to open
-	window.location.href =
-		"https://drive.google.com/file/d/14BoM_buqN56GEp0LiAuzv5TqnQzSmDGw/view?usp=sharing"
-})
-
-document.getElementById('githubButton').addEventListener('click', function () {
-	window.location.href = "https://github.com/LouSkull/EasyWindowsTool"
-})
